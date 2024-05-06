3f78ee8443c4bcc7c7c0e40997aea3c6fe1b8e4b
Fetra Harinjatovo <fetra.harinjatovo@pulse.mg>
2024-05-06 15:41:19 +0300

    Abstract Format Negociator

--- Negociation/FormatNegociator.php
+++ Negociation/FormatNegociator.php
@@ -5,49 +5,12 @@ namespace Tounaf\Exception\Negociation;
 use Symfony\Component\HttpFoundation\Request;
 use Tounaf\Exception\RequestMatcher\MatcherRequestInterface;
 
-class FormatNegociator
+abstract class FormatNegociator
 {
     /**
-     * @var Request $request
+     * Retour the best format from the given request
+     * @return string
      */
-    private $request;
+    abstract public function getBest(): ?string;
 
-    /**
-     * @var array
-     */
-    private $map = [];
-
-    public function add(MatcherRequestInterface $requestMatcherInterface, array $options = [])
-    {
-        $this->map[] = [$requestMatcherInterface, $options];
-    }
-
-    public function getBest(): ?string
-    {
-        $request = $this->getRequest();
-        foreach ($this->map as $elements) {
-            // Check if the current RequestMatcherInterface matches the current request
-            if (!$elements[0]->matches($request)) {
-                continue;
-            }
-
-            return $elements[1]['format'];
-        }
-
-        return null;
-    }
-
-    public function setRequest(Request $request)
-    {
-        $this->request = $request;
-    }
-
-    private function getRequest()
-    {
-        if(null === $this->request) {
-            throw new \RuntimeException("There is no current request");
-        }
-
-        return $this->request;
-    }
 }
