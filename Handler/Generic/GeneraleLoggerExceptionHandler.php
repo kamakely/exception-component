20cd85aec6f492a866209410ee398dac7043f6a4
bensonirah <bensonirah@gmail.com>
2024-06-18 22:17:24 +0300

    Add symfony/error-handler dependecies and move support function in base class

--- Handler/Generic/GeneraleLoggerExceptionHandler.php
+++ Handler/Generic/GeneraleLoggerExceptionHandler.php
@@ -4,80 +4,82 @@ namespace Tounaf\Exception\Handler\Generic;
 
 use Psr\Log\LoggerInterface;
 use ReflectionObject;
+use Symfony\Component\ErrorHandler\Exception\FlattenException;
 use Symfony\Component\HttpFoundation\Exception\RequestExceptionInterface;
 use Tounaf\Exception\Exception\ExceptionHandlerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Tounaf\Exception\Exception\AbstractExceptionHandler;
-use Tounaf\Exception\Exception\TounafException;
 
 class GeneraleLoggerExceptionHandler extends AbstractExceptionHandler
 {
-	private LoggerInterface $logger;
+    private LoggerInterface $logger;
 
 
-	public function __construct(LoggerInterface $logger)
-	{
-		$this->logger = $logger;
-	}
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
 
-	public function handleException(\Throwable $throwable): Response
-	{
-		$e = FlattenException::createFromThrowable($throwable);
+    public function handleException(\Throwable $throwable): Response
+    {
+        $e = FlattenException::createFromThrowable($throwable);
 
-		$handlers = $this->extractHandler($this);
-
-
-		$this->logException(
-			$throwable, sprintf(
-			'Uncaught PHP Exception %s: "%s" at %s line %s', $e->getClass(), $e->getMessage(), $e->getFile(),
-			$e->getLine()
-		),  $handlers
-		);
-
-		return $this->decoratedExceptionHandlerInterface->handleException($throwable);
-	}
-
-	public function supportsException(\Throwable $throwable): bool
-	{
-		return $throwable instanceof TounafException;
-	}
+        $handlers = $this->extractHandler($this);
 
 
-	private function extractHandler(ExceptionHandlerInterface $handler): array
-	{
-		$handlers   = [];
-		$reflection = new ReflectionObject($handler);
-		$properties = $reflection->getProperties();
+        $this->logException(
+            $throwable,
+            sprintf(
+                'Uncaught PHP Exception %s: "%s" at %s line %s',
+                $e->getClass(),
+                $e->getMessage(),
+                $e->getFile(),
+                $e->getLine()
+            ),
+            $handlers
+        );
 
-		foreach ($properties as $property) {
-			$property->setAccessible(true);
-			$h = $property->getValue($handler);
-			if ($h instanceof ExceptionHandlerInterface) {
-				$handlers [] = get_class($h);
-				$handlers [] = $this->extractHandler($h);
-			}
-		}
-
-		return $handlers;
-
-	}
+        return $this->decoratedExceptionHandlerInterface->handleException($throwable);
+    }
 
 
-	protected function logException(\Throwable $exception, $message, array $handlers = []): void
-	{
-		$context = ['exception' => $exception, 'handlers' => $handlers];
-		if (!$exception instanceof RequestExceptionInterface) {
-			$this->logger->critical(
-				$message,
-				$context
-			);
-		}
-		else {
-			$this->logger->error(
-				$message,
-				$context
-			);
-		}
-	}
+    private function extractHandler(ExceptionHandlerInterface $handler): array
+    {
+        $handlers = [];
+        $reflection = new ReflectionObject($handler);
+        $properties = $reflection->getProperties();
 
+        foreach ($properties as $property) {
+            $property->setAccessible(true);
+            $h = $property->getValue($handler);
+            if ($h instanceof ExceptionHandlerInterface) {
+                $handlers [] = get_class($h);
+                $handlers [] = $this->extractHandler($h);
+            }
+        }
+
+        return $handlers;
+    }
+
+
+    protected function logException(\Throwable $exception, $message, array $handlers = []): void
+    {
+        $context = ['exception' => $exception, 'handlers' => $handlers];
+        if (!$exception instanceof RequestExceptionInterface) {
+            $this->logger->critical(
+                $message,
+                $context
+            );
+        } else {
+            $this->logger->error(
+                $message,
+                $context
+            );
+        }
+    }
+
+    public function supportsException(\Throwable $throwable): bool
+    {
+        return true;
+    }
 }
